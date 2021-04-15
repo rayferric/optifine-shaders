0a39eac4b85112a644888ecf5677b90b17160690
rayferric <63957587+rayferric@users.noreply.github.com>
2021-04-15 09:37:31 +0200

    Improve tone mapping

--- shaders/final.fsh
+++ shaders/final.fsh
@@ -1,29 +1,14 @@
 #version 120
 
 #include "include/common.glsl"
+
 #include "include/encoding.glsl"
+#include "include/luminance.glsl"
+#include "include/tonemap.glsl"
 
 varying vec2 v_TexCoord;
 
 uniform sampler2D colortex1;
-
-/**
- * Transforms HDR color to LDR using ACES operator.
- * Ported from original source:
- * https://knarkowicz.wordpress.com/2016/01/06/aces-filmic-tone-mapping-curve
- *
- * @param color HDR color
- *
- * @return LDR color
- */
- vec3 tonemapACES(in vec3 color) {
-	const float a = 2.51;
-	const float b = 0.03;
-	const float c = 2.43;
-	const float d = 0.59;
-	const float e = 0.14;
-	return clamp((color * (a * color + b)) / (color * (c * color + d) + e), 0.0, 1.0);
-}
 
 uniform sampler2D depthtex0;
 
@@ -33,7 +18,7 @@ void main() {
 
 	vec3 color = tonemapACES(hdr);
 	color = pow(color, vec3(GAMMA));
-	color = clamp(mix(vec3(luma(color)), color, SATURATION), 0.0, 1.0);
+	color = clamp(mix(vec3(luminance(color)), color, SATURATION), 0.0, 1.0);
 	color = clamp(mix(vec3(0.5), color, CONTRAST), 0.0, 1.0);
 	
 	gl_FragColor.xyz = linearToGamma(color);
---
+++ shaders/include/tonemap.glsl
@@ -0,0 +1,88 @@
+#ifndef TONEMAP_GLSL
+#define TONEMAP_GLSL
+
+#include "encoding.glsl"
+
+// sRGB => XYZ => D65_2_D60 => AP1 => RRT_SAT
+const mat3 ACESIn = mat3(
+    0.59719, 0.07600, 0.02840,
+    0.35458, 0.90834, 0.13383,
+    0.04823, 0.01566, 0.83777
+);
+
+// ODT_SAT => XYZ => D60_2_D65 => sRGB
+const mat3 ACESOut = mat3(
+     1.60475, -0.10208, -0.00327,
+    -0.53108,  1.10813, -0.07276,
+    -0.07367, -0.00605,  1.07602
+);
+
+vec3 RTTAndODTFit(in vec3 color) {
+    vec3 a = color * (color + 0.0245786) - 0.000090537;
+    vec3 b = color * (0.983729 * color + 0.4329510) + 0.238081;
+    return a / b;
+}
+
+/**
+ * Transforms HDR color to LDR using ACES operator.
+ * Ported from original source by Stephen Hill:
+ * https://github.com/TheRealMJP/BakingLab/blob/master/BakingLab/ACES.hlsl
+ * https://github.com/TheRealMJP/BakingLab/blob/master/BakingLab/ToneMapping.hlsl
+ *
+ * @param hdr HDR color
+ *
+ * @return LDR color
+ */
+vec3 tonemapACES(in vec3 color) {
+	color *= 1.8;
+    color = ACESIn * color;
+	color = RTTAndODTFit(color);
+    color = ACESOut * color;
+	return clamp(color, 0.0, 1.0);
+}
+
+/**
+ * Transforms HDR color to LDR using ACES operator approximation.
+ * Ported from original source by Krzysztof Narkowicz:
+ * https://knarkowicz.wordpress.com/2016/01/06/aces-filmic-tone-mapping-curve
+ *
+ * @param hdr HDR color
+ *
+ * @return LDR color
+ */
+vec3 tonemapApproxACES(in vec3 hdr) {
+	const float a = 2.51;
+	const float b = 0.03;
+	const float c = 2.43;
+	const float d = 0.59;
+	const float e = 0.14;
+	return clamp((hdr * (a * hdr + b)) / (hdr * (c * hdr + d) + e), 0.0, 1.0);
+}
+
+/**
+ * Transforms HDR color to LDR using modified Reinhard
+ * operator that mimics ACES curve with infinite white point.
+ * This function enhances TAA moving average results.
+ *
+ * @param hdr HDR color
+ *
+ * @return LDR color
+ */
+vec3 tonemapModifiedReinhard(in vec3 hdr) {
+	return hdr / (hdr + 0.25);
+}
+
+/**
+ * Transforms LDR color to HDR using inverse of modified Reinhard
+ * operator that mimics ACES curve with infinite white point.
+ * This function enhances TAA moving average results.
+ *
+ * @param hdr LDR color
+ *
+ * @return HDR color
+ */
+vec3 tonemapModifiedReinhardInverse(in vec3 ldr) {
+	return (0.25 * ldr) / (1.0 - ldr);
+}
+
+#endif // TONEMAP_GLSL
